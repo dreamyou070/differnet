c145e42e0637b08419e2d138802ee81fa88a723a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-29 13:10:32 +0900

    Update model.py

--- model.py
+++ model.py
@@ -66,7 +66,7 @@ class DifferNet(nn.Module):
     def forward(self, x):
         y_cat = list()
 
-        for s in range(c.n_scales):
+        for s in range(self.n_scales):
             x_scaled = F.interpolate(x, size=c.img_size[0] // (2 ** s)) if s > 0 else x
             feat_s = self.feature_extractor.features(x_scaled)
             y_cat.append(torch.mean(feat_s, dim=(2, 3)))
